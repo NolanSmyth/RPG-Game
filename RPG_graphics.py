70ced02abc3c00a7a017b709b76695432cb020c4
NolanSmyth <Nolanwsmyth@gmail.com>
2017-05-08 10:34:52 +0100

    graphics updates
    Improving pygame graphics and functionality

--- RPG_graphics.py
+++ RPG_graphics.py
@@ -1,9 +1,11 @@
 import sys, pygame
 from pygame.locals import *
 
+movement_speed = 10
+
 # Initialize screen
 pygame.init()
-size = [width, height] = [600, 400]
+size = [width, height] = [800, 600]
 screen = pygame.display.set_mode(size)
 white = [250, 250, 250]
 speed = [8,3]
@@ -13,32 +15,48 @@ def main():
 	background_img = pygame.image.load("background.jpg")
 	background = pygame.Surface(screen.get_size())
 	background = background.convert()
-	background.fill(white)
+	# background.fill(white)
 
 	# Display text
 
-	font = pygame.font.Font(None, 40)
-	text = font.render("Euler's Quest", True, [10,10,10])
-	textpos = text.get_rect()
-	textpos.centerx = background.get_rect().centerx
-	textpos.centery = background.get_rect().centery
+	# font = pygame.font.Font(None, 40)
+	# text = font.render("Euler's Quest", True, [10,10,10])
+	# textpos = text.get_rect()
+	# textpos.centerx = background.get_rect().centerx
+	# textpos.centery = background.get_rect().centery
 	
-		
+	# Character Movement
+	player = pygame.image.load("ball.png")
+	player_pos = Rect(50,50,128,128)
+	
+
 
 	# Event loop
 	while True:
-		if textpos.left <= 0 or textpos.right >= width:
-			speed[0] = -speed[0]
-		if textpos.top <= 0 or textpos.bottom >= height:
-			speed[1] = -speed[1]
-		screen.blit(background_img, text.get_rect())
-		textpos = textpos.move(speed)
-		screen.blit(text, textpos)
-		pygame.display.update(textpos)
-		pygame.time.delay(100)
+		
+		# textpos = textpos.move(speed)
+		# screen.blit(text, textpos)
+		# pygame.display.update(textpos)
+
+		key = pygame.key.get_pressed()
+		if key[pygame.K_LEFT]:
+			player_pos.left -= movement_speed
+		if key[pygame.K_RIGHT]:
+			player_pos.left += movement_speed
+		if key[pygame.K_UP]:
+			player_pos.top -= movement_speed
+		if key[pygame.K_DOWN]:
+			player_pos.bottom += movement_speed
+		pygame.time.delay(30)
+
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				return
+
+		background.blit(background_img, (0,0))
+		background.blit(player,player_pos)
+		screen.blit(background,(0,0))
+		pygame.display.flip()
 
 	    
 
